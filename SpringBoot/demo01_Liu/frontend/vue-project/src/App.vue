052e67839c4bb4c01c846b165bb71918a8915ea0
OnetoTommy <xingguangc09@gmail.com>
2025-10-15 18:51:58 -0700

    Vue + Element

---
+++ SpringBoot/demo01_Liu/frontend/vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Movie v-for="movie in movies" :key="movie.id" :title="movie.title" :rating="movie.rating"></Movie>
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Movie from './components/Movie.vue'
+import Hello from './components/Hello.vue'
+export default {
+  name: 'App',
+  data() {
+    return {
+      movies: [
+        { id: 1, title: '金刚狼', rating: 8.7 },
+        { id:2, title: '速度与激情', rating: 9.2 },
+        { id:3, title: '复仇者联盟', rating: 9.5 }
+      ]
+    }
+  },
+  components: {
+    Movie,
+    Hello
+  }
+}
+</script>
+
+<style >
+
+</style>
---
+++ SpringBoot/demo01_Liu/frontend/vue-project/src/components/Movie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <span>{{ rating }}</span>
+    <button @click="functionA">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Movie',
+  props: ["title", "rating"],
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    functionA() {
+      alert('Function A called');
+    }
+  }
+}
+</script>
